5097de233c55cdc345e31e5264f467a3b0ebe87d
DrGrenkenstien <shubhambhadoria10@gmail.com>
2023-11-20 11:10:36 +0530

    added tailwind css

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { loginFields } from "../constants/formFields";
+import FormAction from "./FormAction";
+import FormExtra from "./FormExtra";
+import Input from "./Input";
+import auth from "../constants/firebase"
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+
+const fields=loginFields;
+let fieldsState = {};
+fields.forEach(field=>fieldsState[field.id]='');
+
+export default function Login(){
+    const [loginState,setLoginState]=useState(fieldsState);
+
+    const handleChange=(e)=>{
+        setLoginState({...loginState,[e.target.id]:e.target.value})
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        authenticateUser();
+    }
+
+    //Handle Login API Integration here
+    const authenticateUser = () =>{
+        const email = loginState["email-address"]
+        const password = loginState["password"]
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+    }
+
+    return(
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+        <div className="-space-y-px">
+            {
+                fields.map(field=>
+                        <Input
+                            key={field.id}
+                            handleChange={handleChange}
+                            value={loginState[field.id]}
+                            labelText={field.labelText}
+                            labelFor={field.labelFor}
+                            id={field.id}
+                            name={field.name}
+                            type={field.type}
+                            isRequired={field.isRequired}
+                            placeholder={field.placeholder}
+                    />
+                
+                )
+            }
+        </div>
+
+        <FormExtra/>
+        <FormAction handleSubmit={handleSubmit} text="Login"/>
+
+      </form>
+    )
+}
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Nav from '../components/Nav'
+
+const Home = () => {
+  return (
+    <>
+    <h1>Nav goes here</h1>
+    </>
+  )
+}
+
+export default Home
