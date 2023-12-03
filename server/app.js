8ea189770305e1f9c03707ca14860f0b1fe9fb54
DrGrenkenstien <shubhambhadoria10@gmail.com>
2023-12-03 16:38:23 +0530

    successfully connected to db

--- server/app.js
+++ server/app.js
@@ -2,16 +2,16 @@ import express from "express";
 import cors from 'cors'
 import mongoose from "mongoose";
 import UserModel from "./Schema/Users.js"
+mongoose.connect("mongodb+srv://Shub:zUtCyGkYncWPySJL@cluster0.zmwunml.mongodb.net/CodeForcesHelper?retryWrites=true&w=majority").then(() => {console.log("Connected")}).catch((e) => {console.log("Error", e)})
 
-const conn = await mongoose.createConnection("mongodb+srv://Shub:zUtCyGkYncWPySJL@cluster0.zmwunml.mongodb.net/CodeForcesHelper?retryWrites=true&w=majority", {
-    serverSelectionTimeoutMS: 30000, // Increase server selection timeout
-    socketTimeoutMS: 45000, // Increase socket timeout
-  }).asPromise()
-
-console.log(conn.readyState == 1 ? "Connected successfully to MongoDB server" : "Failed to connect to MongoDB server")
+// console.log(conn.readyState)
 
 const app = express()
 const port = 3000
+
+const person = await UserModel.findOne({});
+console.log(person.email)
+console.log(person)
 
 app.use(cors())
 
@@ -25,8 +25,19 @@ const Save = async (newUser) => {
 }
 
 app.post("/user/profile", (req, res) => {
-    const newUser = new UserModel({"email" : res["email"], "codeforces_username" : res["username"]})
-    Save(newUser)
+    // const newUser = new UserModel({"email" : res["email"], "codeforces_username" : res["username"]})
+    const ans = UserModel.countDocuments({})
+    console.log(ans)
+    // UserModel.find({}, (err, doc) => {
+    //     if (err) {
+    //         console.error(err);
+    //       } else {
+    //         console.log('All documents in the collection:', documents);
+    //       }
+    // })
+    // console.log(res)
+    
+    // Save(newUser)
 })
 
 app.listen(port, () => {
